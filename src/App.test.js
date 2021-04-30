0d7403e3ddc87e56f5dd507c4959ec8e377c4ba8
israel diaz <israelmyname@gmail.com>
2021-04-29 22:41:14 -0500

    :sparkles: commit decisivo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { create } from 'react-test-renderer'; 
+import App from './App'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let component;
+
+describe('<App />', ()=> {
+  
+  beforeEach(()=> {
+    component = create(<App/>);
+
+  });
+
+  //casos de prueba my friend
+    it('Renderización de componente App', ()=> {
+      expect(component).toBeDefined();
+    })
+
+    it('se renderiza header tag', () => {
+      const header = component.root.findByType('header');
+      expect(header).toBeDefined();
+      expect(header.props.className).toEqual('App-header');
+    });
+    
+    it('tag header tiene elemento img', () => {
+      const image = component.root.findByType('img');
+      expect(image).toBeDefined();
+      expect(image.props.className).toEqual('App-logo');
+    });
+
+    test('tag tiene su clase bien escrita', ()=>{
+      const anchor = component.root.findByType('a');
+      expect(anchor).toBeDefined();
+      expect(anchor.props.className).toEqual('App-link');
+
+    });
 });
